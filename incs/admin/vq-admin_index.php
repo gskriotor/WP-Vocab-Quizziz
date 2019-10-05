942650b2cd91c8785d2888e0870f4b8743f181ab
gskriptor <gspencer.create@gmail.com>
2019-10-04 19:30:19 -0700

    adding file structure

---
+++ incs/admin/vq-admin_index.php
@@ -0,0 +1,12 @@
+function q_adminTab() {
+     add_menu_page(
+      'Quiz Result Q',
+      'Quiz Result Q',
+      'edit_posts',
+      'quiz_result',
+      'result_finder',
+      'dashicons-analytics'
+
+     );
+}
+add_action('admin_menu', 'q_adminTab');
