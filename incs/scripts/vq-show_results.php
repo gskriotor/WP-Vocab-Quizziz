9ce929516d7df5736cab7fd84bebc2f51a3e0bba
gskriptor <54649017+gskriptor@users.noreply.github.com>
2019-10-05 21:33:24 -0700

    Update vq-show_results.php

--- incs/scripts/vq-show_results.php
+++ incs/scripts/vq-show_results.php
@@ -1,6 +1,6 @@
 <?php
 
-function vq-get_studs() {
+function vq_show_results() {
 
     $pName = $_POST['exam'];
 
@@ -38,29 +38,6 @@ function vq-get_studs() {
     echo 'ready to print';
 
     echo '
-    <style type="text/css">
-    	.TFtable{
-    		width:100%;
-    		border-collapse:collapse;
-    	}
-    	.TFtable td{
-    		padding:7px; border:#4e95f4 1px solid;
-    	}
-    	/* provide some minimal visual accomodation for IE8 and below */
-    	.TFtable tr{
-    		background: #b8d1f3;
-    	}
-    	/*  Define the background color for all the ODD background rows  */
-    	.TFtable tr:nth-child(odd){
-    		background: #b8d1f3;
-    	}
-    	/*  Define the background color for all the EVEN background rows  */
-    	.TFtable tr:nth-child(even){
-    		background: #dae5f4;
-    	}
-
-    </style>
-    ';
 
    echo '<center><h2>Full Student List</h2></center>';
     echo '<table class="TFtable">';
@@ -217,11 +194,6 @@ echo '<td>total correct answers per question: '.$totalCorrAnsw_perQuest.'</td>';
  echo '</table>';
   }
 echo '</div>';
-}
-
-function vq-show_results() {
-   vq-get_studs();
-   vq-get_exam();
 }
 
 ?>
