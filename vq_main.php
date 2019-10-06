020147b89c596a8221ec4e3a6c3c2962febb3624
gskriptor <54649017+gskriptor@users.noreply.github.com>
2019-10-05 21:20:05 -0700

    Update vq_main.php

--- vq_main.php
+++ vq_main.php
@@ -9,6 +9,17 @@ Author URI: https://gusspencer.com
 Text Domain: education
 */
 
+if(!defined('ABSPATH')) {
+   exit;
+}
+
+require_once(plugin_dir_path(__FILE__) . '/incs/scripts/vq-funcs_head.php');
+require_once(plugin_dir_path(__FILE__) . '/incs/scripts/vq-post_results.php');
+require_once(plugin_dir_path(__FILE__) . '/incs/scripts/vq-show_results.php');
+require_once(plugin_dir_path(__FILE__) . '/incs/admin/vq-admin_index.php');
+require_once(plugin_dir_path(__FILE__) . '/incs/views/vq-view_form.php');
+require_once(plugin_dir_path(__FILE__) . '/admin_index.php');
+
 
 
 function result_finder() {
