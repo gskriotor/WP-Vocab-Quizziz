6fd5026440a2bb7ea80b2a504b10e20d7c3736df
gskriptor <54649017+gskriptor@users.noreply.github.com>
2019-10-05 20:35:01 -0700

    Update vq-post_results.php

--- incs/scripts/vq-post_results.php
+++ incs/scripts/vq-post_results.php
@@ -39,30 +39,7 @@ $pName = $_POST['exam'];
    $lowScorePercCl = number_format($lowScorePerc, 2);
 //echo 'ready to print';
 
-echo '
-<style type="text/css">
-	.TFtable{
-		width:100%;
-		border-collapse:collapse;
-	}
-	.TFtable td{
-		padding:7px; border:#4e95f4 1px solid;
-	}
-	/* provide some minimal visual accomodation for IE8 and below */
-	.TFtable tr{
-		background: #b8d1f3;
-	}
-	/*  Define the background color for all the ODD background rows  */
-	.TFtable tr:nth-child(odd){
-		background: #b8d1f3;
-	}
-	/*  Define the background color for all the EVEN background rows  */
-	.TFtable tr:nth-child(even){
-		background: #dae5f4;
-	}
-
-</style>
-';
+//none of these commented out worked
 //$res_p = do_shortcode('[printfriendly]');
 //$res_p = apply_filters( 'the_content',' [printfriendly] ');
 //$res_p = '<button class="wp-block-button__link"><a href="https://www.printfriendly.com/p/g/N8EMJH">Print / PDF</a></button>';
