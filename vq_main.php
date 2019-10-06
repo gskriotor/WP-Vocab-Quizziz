1369c1fbfa1c61db81e87d58778c32c41af0d5fd
gskriptor <54649017+gskriptor@users.noreply.github.com>
2019-10-05 21:37:21 -0700

    Update vq_main.php

--- vq_main.php
+++ vq_main.php
@@ -8,20 +8,22 @@ Author Gus Spencer
 Author URI: https://gusspencer.com
 Text Domain: education
 */
-
-
-
+if(!defined('ABSPATH')) {
+   exit;
+}
+//require_once(plugin_dir_path(__FILE__) . '/incs/scripts/vq-funcs_head.php');
+require_once(plugin_dir_path(__FILE__) . '/incs/scripts/vq-post_results.php');
+require_once(plugin_dir_path(__FILE__) . '/incs/scripts/vq-show_results.php');
+//require_once(plugin_dir_path(__FILE__) . '/incs/admin/vq-admin_index.php');
+require_once(plugin_dir_path(__FILE__) . '/incs/views/vq-view_form.php');
 function result_finder() {
-
    finder_form();
    if(isset($_POST['post_results'])) {
      post_results();
    }
    if(isset($_POST['submit'])) {
-      get_exam();
-      get_studs();
+      vq_show_results();
    }
 }
-
-
 ?>
+© 2019 GitHub, Inc.
