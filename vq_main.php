ebe856a606188f0bc376ea14155d9413711d7467
gskriptor <54649017+gskriptor@users.noreply.github.com>
2019-10-05 21:33:57 -0700

    Update vq_main.php

--- vq_main.php
+++ vq_main.php
@@ -16,7 +16,7 @@ if(!defined('ABSPATH')) {
 require_once(plugin_dir_path(__FILE__) . '/incs/scripts/vq-funcs_head.php');
 require_once(plugin_dir_path(__FILE__) . '/incs/scripts/vq-post_results.php');
 require_once(plugin_dir_path(__FILE__) . '/incs/scripts/vq-show_results.php');
-require_once(plugin_dir_path(__FILE__) . '/incs/admin/vq-admin_index.php');
+//require_once(plugin_dir_path(__FILE__) . '/incs/admin/vq-admin_index.php');
 require_once(plugin_dir_path(__FILE__) . '/incs/views/vq-view_form.php');
 
 function result_finder() {
@@ -26,10 +26,8 @@ function result_finder() {
      post_results();
    }
    if(isset($_POST['submit'])) {
-      get_exam();
-      get_studs();
+      vq_show_results();
    }
 }
-
 
 ?>
